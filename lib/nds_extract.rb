f52843d69cc0276e3e5efbbfe04caf79eb155bd5
Andrew de la Vega <andrew21delavega@gmail.com>
2020-03-03 09:03:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,15 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  names = []
+  source.map{ |d| d[:name] }	  i = 0
+
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+
+  names
 end
 
 def total_gross(source)
